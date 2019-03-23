4fe5d108eb956457a2cfe74345b67e123c95ad26
Tin Nik <tinnik@protonmail.com>
2019-03-23 15:00:02 +1000

    Added Twitch

---
+++ scripts/twitch.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name     	Twitch
+// @namespace	https://tarinnik.github.io/gmscripts
+// @version  	1
+// @include  	https://www.twitch.tv/*
+// @grant    	none
+// ==/UserScript==
+
+
+//Toggle theatre mode
+function theatre_mode() {
+  document.getElementsByClassName("player-button qa-theatre-mode-button")[0].click();
+}
+
+//Toggle fullscreen mode
+function fullscreen_mode() {
+  document.getElementsByClassName("player-button qa-fullscreen-button pl-mg-r-1 pl-button__fullscreen--tooltip-left")[0].click();
+}
+
+// Key mappings
+var map = {};
+onkeydown = onkeyup = function(e){
+  map[e.keyCode] = e.type == 'keydown';
+
+  // CTRL + ALT + T - theatre mode
+  if (map[17] && map[18] && map[84]) {
+    theatre_mode();
+  }
+}
