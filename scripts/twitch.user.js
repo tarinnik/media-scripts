4d09eda956e55cff9a53c943e4fd2c561baa584b
Tin Nik <tinnik@protonmail.com>
2019-06-25 16:31:01 +1000

    Added video select in vod section

--- scripts/twitch.user.js
+++ scripts/twitch.user.js
@@ -1,29 +1,115 @@
 // ==UserScript==
-// @name     	Twitch
-// @namespace	tarinnik.gitlab.io/gmscripts
-// @version  	1
-// @include  	https://www.twitch.tv/*
+// @name 		Twitch
+// @namespace 	tarinnik.gitlab.io/gmscripts
+// @version 	1.1
+// @include 	https://www.twitch.tv/*
 // @icon 		https://twitch.tv/favicon.ico
 // ==/UserScript==
 
+if (typeof video_select === 'undefined') {
+	var video_select = -1;
+}
+
+var selectionColour = "background:#4b367c";
 
 //Toggle theatre mode
 function theatre_mode() {
-  document.getElementsByClassName("player-button qa-theatre-mode-button")[0].click();
+	document.getElementsByClassName("player-button qa-theatre-mode-button")[0].click();
 }
 
 //Toggle fullscreen mode
-function fullscreen_mode() {
-  document.getElementsByClassName("player-button qa-fullscreen-button pl-mg-r-1 pl-button__fullscreen--tooltip-left")[0].click();
+function fullscreen() {
+	document.getElementsByClassName("player-button qa-fullscreen-button pl-mg-r-1 pl-button__fullscreen--tooltip-left")[0].click();
 }
 
 // Key mappings
 var map = {};
 onkeydown = onkeyup = function(e){
-  map[e.keyCode] = e.type == 'keydown';
+	map[e.keyCode] = e.type == 'keydown';
 
-  // CTRL + ALT + T - theatre mode
-  if (map[17] && map[18] && map[84]) {
-    theatre_mode();
-  }
+	// CTRL + ALT + T - theatre mode
+	if (map[17] && map[18] && map[84]) {
+		theatre_mode();
+	}
+
+	// CTRL + ALT + F - Fullscreen
+	if (map[17] && map[18] && map[70]) {
+		fullscreen();
+	}
+
+	// CTRL + ALT + N - Highlight next video/streamer
+	if (map[17] && map[18] && map[78]) {
+
+		//Streamers
+		if (window.location.href == "https://www.twitch.tv/*/videos*") {
+			console.log("Videos");
+		} 
+
+		//Videos
+		else {
+			//If it's the first video
+			if (video_select == -1) {
+				video_select++;
+				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].setAttribute("style", selectionColour);
+			}
+
+			//If it's the last video
+			else if (video_select >= document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2").length-1 ) {
+				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[0].setAttribute("style", selectionColour);
+				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].removeAttribute("style");
+				video_select = 0;
+			}
+
+			//If it's anything else
+			else {
+				video_select++;
+				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].setAttribute("style", selectionColour);
+				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select-1].removeAttribute("style");
+			}
+
+
+			document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[0].getElementsByClassName("tw-interactive tw-link")	
+		}
+	}
+
+	// CTRL + ALT + M - Highlight previous video/streamer
+	if (map[17] && map[18] && map[77]) {
+		//Streamers
+		if (window.location.href == "https://www.twitch.tv/*/videos*") {
+			console.log("Videos");
+		}
+
+		//Videos
+		else {
+
+			var length = document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2").length;
+
+			//If it's the first video
+			if (video_select == 0 || video_select == -1) {
+				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[length-1].setAttribute("style", selectionColour);
+				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[0].removeAttribute("style");
+				video_select = length-1;
+			}
+
+			else {
+				video_select--;
+				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].setAttribute("style", selectionColour);
+				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select+1].removeAttribute("style");
+			}
+		}
+	}
+
+	// CTRL + ALT + S - Select highlighted video
+	if (map[17] && map[18] && map[83]) {
+		//Streamers
+		if (window.location.href == "https://www.twitch.tv/*/videos*") {
+			console.log("Videos");
+		}
+
+		//Videos
+		else {
+			document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].getElementsByClassName("tw-interactive tw-link")[0].click();
+		}
+	}
+
 }
