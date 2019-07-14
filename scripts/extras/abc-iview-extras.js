a4daa420087d8f2e0b89171afb7c10bee941581c
Tin Nik <tinnik@protonmail.com>
2019-07-14 15:41:11 +1000

    Remove dead files

--- scripts/extras/abc-iview-extras.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name     	ABC iview extras
-// @namespace 	tarinnik.gitlab.io/gmscripts
-// @version  	0.1
-// @include  	https://iview.abc.net.au/your/recents/
-// @icon		https://iview.abc.net.au/favicon.ico
-// ==/UserScript==
-
-document.getElementsByClassName("iv-1fREI")[0].scrollIntoView();
