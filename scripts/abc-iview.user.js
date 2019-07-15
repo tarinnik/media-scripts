8898f4df8ad9f18d3922f0a9713a021e227c6824
Tin Nik <tinnik@protonmail.com>
2019-07-15 10:16:16 +1000

    Fix scrolling

--- scripts/abc-iview.user.js
+++ scripts/abc-iview.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		ABC iview
 // @namespace	tarinnik.gitlab.io/gmscripts
-// @version		2.1.5
+// @version		2.1.6
 // @include		https://iview.abc.net.au/*
 // @icon		https://iview.abc.net.au/favicon.ico
 // ==/UserScript==
@@ -128,7 +128,7 @@ onkeydown = onkeyup = function(e){
 
 		else if(window.location.href.slice(0,30) == "https://iview.abc.net.au/show/") {
 			selectPrevious("iv-2Nzsw");
-			if (!!document.getElementsByClassName("iv-hsfpe")) {
+			if (!!document.getElementsByClassName("iv-hsfpe")[0]) {
 				scroll(video_select, "iv-hsfpe", "iv-2Nzsw", 1);
 			} else {
 				scroll(video_select, "iv-x90Qp", "iv-2Nzsw", 1);
