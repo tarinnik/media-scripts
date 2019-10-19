38b431efd9d5468f75297adaf75bc6d834770856
Tin Nik <tinnik@protonmail.com>
2019-10-19 17:35:26 +1000

    Fixed theatre mode and icon

--- scripts/twitch.user.js
+++ scripts/twitch.user.js
@@ -1,34 +1,61 @@
 // ==UserScript==
-// @name 	Twitch
-// @namespace 	tarinnik.gitlab.io/gmscripts
-// @version 	1.4.2
-// @include 	https://www.twitch.tv/*
-// @icon 	https://twitch.tv/favicon.ico
+// @name        Twitch
+// @namespace   tarinnik.gitlab.io/gmscripts
+// @version     1.4.3
+// @include     https://www.twitch.tv/*
+// @icon        https://static.twitchcdn.net/assets/favicon-32-d6025c14e900565d6177.png
 // ==/UserScript==
 
-const THEATRE_MODE_INDEX = 1;
+const BOTTOM_RIGHT_NAV_ARRAY = document.getElementsByClassName(
+		"player-controls__right-"+
+		"control-group tw-align-items-center tw-flex tw-flex-grow-1 tw-justify"+
+		"-content-end")[0].
+		getElementsByClassName("tw-align-items-center "+
+		"tw-align-middle tw-border-bottom-left-radius-medium tw-border-"+
+		"bottom-right-radius-medium tw-border-top-left-radius-medium "+
+		"tw-border-top-right-radius-medium tw-button-icon tw-button-icon"+
+		"--overlay tw-core-button tw-core-button--border tw-core-button--"+
+		"overlay tw-inline-flex tw-interactive tw-justify-content-center "+
+		"tw-overflow-hidden tw-relative");
 
+const SELECTION_COLOUR = "background:#4b367c";
+
+let video_select;
 if (typeof video_select === 'undefined') {
-	var video_select = -1;
+	video_select = -1;
 }
 
 var selectionColour = "background:#4b367c";
 
-//Toggle theatre mode
+// Toggle theatre mode
 function theatre_mode() {
-	document.getElementsByClassName("player-controls__right-control-group tw-align-items-center tw-flex tw-flex-grow-1 tw-justify-content-end")[0].getElementsByClassName("tw-align-items-center tw-align-middle tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-button-icon tw-button-icon--overlay tw-core-button tw-core-button--border tw-core-button--overlay tw-inline-flex tw-interactive tw-justify-content-center tw-overflow-hidden tw-relative")[THEATRE_MODE_INDEX].click();
+	let THEATRE_MODE_INDEX;
+	if (BOTTOM_RIGHT_NAV_ARRAY.length === 4) {
+		THEATRE_MODE_INDEX = 2;
+	} else {
+		THEATRE_MODE_INDEX = 3;
+	}
+	BOTTOM_RIGHT_NAV_ARRAY[THEATRE_MODE_INDEX].click();
 }
 
-//Toggle fullscreen mode
+/*
+// Toggle fullscreen mode
 function fullscreen() {
-	document.getElementsByClassName("player-button qa-fullscreen-button pl-mg-r-1 pl-button__fullscreen--tooltip-left")[0].click();
+	let FULLSCREEN_INDEX;
+	if (BOTTOM_RIGHT_NAV_ARRAY.length === 4) {
+		FULLSCREEN_INDEX = 3;
+	} else {
+		FULLSCREEN_INDEX = 4;
+	}
+	BOTTOM_RIGHT_NAV_ARRAY[FULLSCREEN_INDEX].click();
 }
+ */
 
-//Recent videos for streamer
+// Recent videos for streamer
 function recents() {
-	var url = window.location.href;
+	const url = window.location.href;
 
-	if (url == "https://www.twitch.tv/") {
+	if (url === "https://www.twitch.tv/") {
 		window.location = "https://twitch.tv/xqcow/videos?filter=archives&sort=time";
 	}
 
@@ -41,27 +68,31 @@ function recents() {
 // Key mappings
 var map = {};
 onkeydown = onkeyup = function(e){
-	map[e.keyCode] = e.type == 'keydown';
+	map[e.keyCode] = e.type === 'keydown';
 
 	// CTRL + ALT + T - theatre mode
 	if (map[17] && map[18] && map[84]) {
 		theatre_mode();
 	}
 
-	// CTRL + ALT + F - Fullscreen
-	if (map[17] && map[18] && map[70]) {
+	/* Use F instead
+	// CTRL + ALT + F- Fullscreen
+	if (map[17] && map[18] && map[83]) {
 		fullscreen();
 	}
+	 */
 
 	// CTRL + ALT + R - Recent videos
 	if (map[17] && map[18] && map[82]) {
 		recents();
 	}
 
-  // CTRL + ALT + O - Search
-  if (map[17] && map[18] && map[79]) {
-      document.getElementById("nav-search-input").focus();
+	/* Use ALT+F instead
+  	// CTRL + ALT + O - Search
+  	if (map[17] && map[18] && map[79]) {
+      document.getElementById("tw-53c90fcb5a12ffa5c59fc299888b334c").focus();
 	}
+	 */
 
 	// CTRL + ALT + N - Highlight next video/streamer
 	if (map[17] && map[18] && map[78]) {
@@ -69,14 +100,14 @@ onkeydown = onkeyup = function(e){
 		//Videos
 		if (window.location.href.includes("videos")) {
 			//If it's the first video
-			if (video_select == -1) {
+			if (video_select === -1) {
 				video_select++;
-				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].setAttribute("style", selectionColour);
+				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].setAttribute("style", SELECTION_COLOUR);
 			}
 
 			//If it's the last video
 			else if (video_select >= document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2").length-1 ) {
-				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[0].setAttribute("style", selectionColour);
+				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[0].setAttribute("style", SELECTION_COLOUR);
 				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].removeAttribute("style");
 				video_select = 0;
 			}
@@ -84,7 +115,7 @@ onkeydown = onkeyup = function(e){
 			//If it's anything else
 			else {
 				video_select++;
-				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].setAttribute("style", selectionColour);
+				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].setAttribute("style", SELECTION_COLOUR);
 				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select-1].removeAttribute("style");
 			}
 
@@ -93,14 +124,14 @@ onkeydown = onkeyup = function(e){
 		//Streamers
 		else {
 			//If it's the first video
-			if (video_select == -1) {
+			if (video_select === -1) {
 				video_select++;
-				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].setAttribute("style", selectionColour);
+				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].setAttribute("style", SELECTION_COLOUR);
 			}
 
 			//If it's the last video
 			else if (video_select >= document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done").length-1 ) {
-				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[0].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].setAttribute("style", selectionColour);
+				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[0].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].setAttribute("style", SELECTION_COLOUR);
 				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].removeAttribute("style");
 				video_select = 0;
 			}
@@ -108,7 +139,7 @@ onkeydown = onkeyup = function(e){
 			//If it's anything else
 			else {
 				video_select++;
-				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].setAttribute("style", selectionColour);
+				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].setAttribute("style", SELECTION_COLOUR);
 				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select-1].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].removeAttribute("style");
 			}
 		}
@@ -123,15 +154,15 @@ onkeydown = onkeyup = function(e){
 			var length = document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2").length;
 
 			//If it's the first video
-			if (video_select == 0 || video_select == -1) {
-				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[length-1].setAttribute("style", selectionColour);
+			if (video_select === 0 || video_select === -1) {
+				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[length-1].setAttribute("style", SELECTION_COLOUR);
 				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[0].removeAttribute("style");
 				video_select = length-1;
 			}
 
 			else {
 				video_select--;
-				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].setAttribute("style", selectionColour);
+				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].setAttribute("style", SELECTION_COLOUR);
 				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select+1].removeAttribute("style");
 			}
 		}
@@ -141,15 +172,15 @@ onkeydown = onkeyup = function(e){
 			var length = document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done").length;
 
 			//If it's the first video
-			if (video_select == 0 || video_select == -1) {
-				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[length-1].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].setAttribute("style", selectionColour);
+			if (video_select === 0 || video_select === -1) {
+				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[length-1].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].setAttribute("style", SELECTION_COLOUR);
 				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[0].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].removeAttribute("style");
 				video_select = length-1;
 			}
 
 			else {
 				video_select--;
-				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].setAttribute("style", selectionColour);
+				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].setAttribute("style", SELECTION_COLOUR);
 				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select+1].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].removeAttribute("style");
 			}
 		}
@@ -165,7 +196,7 @@ onkeydown = onkeyup = function(e){
 		//Streamers
 		else {
 			document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].click();
+			//setTimeout(goToVideos, 2000);
 		}
 	}
-
-}
+};
