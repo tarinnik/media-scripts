997c3c12db0054e6e157a62f7f337adffee90523
Tinnik <tpkoster@protonmail.com>
2019-12-15 12:21:18 +1000

    Init

---
+++ scripts/prime-video.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name     	Prime Video
+// @namespace	tarinnik.github.io/media
+// @version  	0
+// @include		https://www.primevideo.com/*
+// @icon		https://www.primevideo.com/favicon.ico
+// ==/UserScript==
