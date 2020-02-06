74d0aab49e8bafc22aa49ab16a0c09e522882074
Tinnik <tpkoster@protonmail.com>
2020-02-06 14:02:30 +1000

    Reset

--- scripts/twitch.user.js
+++ scripts/twitch.user.js
@@ -1,213 +1,372 @@
 // ==UserScript==
 // @name        Twitch
-// @namespace   tarinnik.gitlab.io/gmscripts
-// @version     1.4.4
+// @namespace   tarinnik.github.io/media
+// @version     0
 // @include     https://www.twitch.tv/*
 // @icon        https://static.twitchcdn.net/assets/favicon-32-d6025c14e900565d6177.png
 // ==/UserScript==
 
-const SELECTION_COLOUR = "background:#4b367c";
+const BACKGROUND_COLOUR = "";
+const HOME_URL = "";
+const LIST_URL = "";
+const SHOW_URL = "";
+const SEARCH_URL = "";
 
-let video_select;
-if (typeof video_select === 'undefined') {
-	video_select = -1;
-}
-
-/*
-function goToVideos() {
-	url = window.location.href;
-	if (url.slice(url.length-6, url.length -1) == "video") {
-		window.location = "/?filter=archives&sort=time";
-	}
-}
- */
-
-// Returns the bottom right navigation bar array
-function getBottomRightNav() {
-	return document.getElementsByClassName(
-		"player-controls__right-"+
-		"control-group tw-align-items-center tw-flex tw-flex-grow-1 tw-justify"+
-		"-content-end")[0].
-		getElementsByClassName("tw-align-items-center "+
-		"tw-align-middle tw-border-bottom-left-radius-medium tw-border-"+
-		"bottom-right-radius-medium tw-border-top-left-radius-medium "+
-		"tw-border-top-right-radius-medium tw-button-icon tw-button-icon"+
-		"--overlay tw-core-button tw-core-button--border tw-core-button--"+
-		"overlay tw-inline-flex tw-interactive tw-justify-content-center "+
-		"tw-overflow-hidden tw-relative");
-}
-
-// Toggle theatre mode
-function theatre_mode() {
-	const BOTTOM_RIGHT_NAV_ARRAY = getBottomRightNav();
-	let THEATRE_MODE_INDEX;
-	if (BOTTOM_RIGHT_NAV_ARRAY.length === 4) {
-		THEATRE_MODE_INDEX = 2;
-	} else {
-		THEATRE_MODE_INDEX = 3;
-	}
-	BOTTOM_RIGHT_NAV_ARRAY[THEATRE_MODE_INDEX].click();
-}
-
-/*
-// Toggle fullscreen mode
-function fullscreen() {
-	let FULLSCREEN_INDEX;
-	if (BOTTOM_RIGHT_NAV_ARRAY.length === 4) {
-		FULLSCREEN_INDEX = 3;
-	} else {
-		FULLSCREEN_INDEX = 4;
-	}
-	BOTTOM_RIGHT_NAV_ARRAY[FULLSCREEN_INDEX].click();
-}
- */
-
-// Recent videos for streamer
-function recents() {
-	const url = window.location.href;
-
-	if (url === "https://www.twitch.tv/") {
-		window.location = "https://twitch.tv/xqcow/videos?filter=archives&sort=time";
-	}
-
-	else {
-		var splitting = url.split("/", 4);
-		window.location = "https://twitch.tv/" + splitting[3] + "/videos?filter=archives&sort=time";
-	}
-}
-
-// Key mappings
-var map = {};
-onkeydown = onkeyup = function(e){
-	map[e.keyCode] = e.type === 'keydown';
-
-	// CTRL + ALT + T - theatre mode
-	if (map[17] && map[18] && map[84]) {
-		theatre_mode();
-	}
-
-	/* Use F instead
-	// CTRL + ALT + F- Fullscreen
-	if (map[17] && map[18] && map[83]) {
-		fullscreen();
-	}
-	 */
-
-	// CTRL + ALT + R - Recent videos
-	if (map[17] && map[18] && map[82]) {
-		recents();
-	}
-
-	/* Use ALT+F instead
-  	// CTRL + ALT + O - Search
-  	if (map[17] && map[18] && map[79]) {
-      document.getElementById("tw-53c90fcb5a12ffa5c59fc299888b334c").focus();
-	}
-	 */
-
-	// CTRL + ALT + N - Highlight next video/streamer
-	if (map[17] && map[18] && map[78]) {
-
-		//Videos
-		if (window.location.href.includes("videos")) {
-			//If it's the first video
-			if (video_select === -1) {
-				video_select++;
-				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].setAttribute("style", SELECTION_COLOUR);
-			}
-
-			//If it's the last video
-			else if (video_select >= document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2").length-1 ) {
-				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[0].setAttribute("style", SELECTION_COLOUR);
-				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].removeAttribute("style");
-				video_select = 0;
-			}
-
-			//If it's anything else
-			else {
-				video_select++;
-				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].setAttribute("style", SELECTION_COLOUR);
-				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select-1].removeAttribute("style");
-			}
-
-		}
-
-		//Streamers
-		else {
-			//If it's the first video
-			if (video_select === -1) {
-				video_select++;
-				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].setAttribute("style", SELECTION_COLOUR);
-			}
-
-			//If it's the last video
-			else if (video_select >= document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done").length-1 ) {
-				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[0].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].setAttribute("style", SELECTION_COLOUR);
-				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].removeAttribute("style");
-				video_select = 0;
-			}
-
-			//If it's anything else
-			else {
-				video_select++;
-				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].setAttribute("style", SELECTION_COLOUR);
-				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select-1].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].removeAttribute("style");
-			}
-		}
-	}
-
-	// CTRL + ALT + M - Highlight previous video/streamer
-	if (map[17] && map[18] && map[77]) {
-
-		//Videos
-		if (window.location.href.includes("videos")) {
-
-			var length = document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2").length;
-
-			//If it's the first video
-			if (video_select === 0 || video_select === -1) {
-				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[length-1].setAttribute("style", SELECTION_COLOUR);
-				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[0].removeAttribute("style");
-				video_select = length-1;
-			}
-
-			else {
-				video_select--;
-				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].setAttribute("style", SELECTION_COLOUR);
-				document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select+1].removeAttribute("style");
-			}
-		}
-
-		//Streamers
-		else {
-			var length = document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done").length;
-
-			//If it's the first video
-			if (video_select === 0 || video_select === -1) {
-				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[length-1].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].setAttribute("style", SELECTION_COLOUR);
-				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[0].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].removeAttribute("style");
-				video_select = length-1;
-			}
-
-			else {
-				video_select--;
-				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].setAttribute("style", SELECTION_COLOUR);
-				document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select+1].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].removeAttribute("style");
-			}
-		}
-	}
-
-	// CTRL + ALT + S - Select highlighted video
-	if (map[17] && map[18] && map[83]) {
-		//Videos
-		if (window.location.href.includes("videos")) {
-			document.getElementsByClassName("tw-flex-wrap tw-tower tw-tower--300 tw-tower--gutter-sm")[0].getElementsByClassName("tw-mg-b-2")[video_select].getElementsByClassName("tw-interactive tw-link")[0].click();
-		}
-
-		//Streamers
-		else {
-			document.getElementsByClassName("tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done")[video_select].getElementsByClassName("side-nav-card tw-align-items-center tw-flex tw-relative")[0].getElementsByClassName("side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05")[0].click();
-			//setTimeout(goToVideos, 2000);
-		}
-	}
+let STATE = {
+	selection: 0,
+	menu: false,
+	videoSelection: 0,
+	season: false,
+	search: false,
+	numSameKeyPresses: 0,
+	lastKeyPressed: '',
+	searchQuery: "",
+	changingChar: '',
 };
+
+const DIRECTION = {
+	remove: 'r',
+	none: 0,
+	forwards: 1,
+	backwards: -1,
+	up: -2,
+	down: 2,
+};
+
+const searchLetters = [
+	[' ', '0'],
+	['q', 'r', 's', '1'],
+	['t', 'u', 'v', '2'],
+	['w', 'x', 'y', 'z', '3'],
+	['g', 'h', 'i', '4'],
+	['j', 'k', 'l', '5'],
+	['m', 'n', 'o', 'p', '6'],
+	['7'],
+	['a', 'b', 'c', '8'],
+	['d', 'e', 'f', '9']
+];
+
+/**
+ * Triggered when the page loads
+ */
+window.addEventListener('load', function() {
+
+});
+
+/**
+ * Triggered when a key is pressed
+ */
+document.addEventListener('keydown', function(event) {
+	key(event);
+});
+
+/**
+ * Determines the key press
+ * @param event that was triggered
+ */
+function key(event) {
+	if (STATE.search) {
+		searchKey(event.key);
+		return;
+	}
+
+	switch (event.key) {
+		case '1':
+			list();
+			break;
+		case '2':
+			down();
+			break;
+		case '3':
+			fullscreen();
+			break;
+		case '4':
+			left();
+			break;
+		case '5':
+			select();
+			break;
+		case '6':
+			right();
+			break;
+		case '7':
+			home();
+			break;
+		case '8':
+			up();
+			break;
+		case '9':
+			back();
+			break;
+		case '0':
+			search();
+			break;
+		case 'Enter':
+			playpause();
+			break;
+		case '.':
+			break;
+		case '+':
+			season();
+			break;
+		case '-':
+			break;
+		case '*':
+			break;
+		case '/':
+			refresh();
+			break;
+	}
+}
+
+/**
+ * Checks if the current page is the home page
+ * @return {boolean} if the current page is the home page
+ */
+function checkHome() {
+
+}
+
+/**
+ * Checks if the current page is the watchlist
+ * @returns {boolean} if the current page is my list
+ */
+function checkList() {
+
+}
+
+/**
+ * Checks if the current page is the show page
+ * @returns {boolean} if the current page is the show page
+ */
+function checkShow() {
+
+}
+
+/**
+ * Checks if the current page is the video
+ * @returns {boolean} if the current page is the video
+ */
+function checkWatch() {
+
+}
+
+/**
+ * Gets the elements that are to be selected
+ */
+function getElements() {
+	if (checkHome()) {
+
+	} else if (checkList()) {
+
+	} else if (checkShow()) {
+
+	}
+}
+
+/**
+ * Gets the number of columns the elements have
+ * @returns {number} number of columns
+ */
+function getColumns() {
+	if (checkHome()) {
+
+	} else {
+		return 1;
+	}
+}
+
+/**
+ * Highlights an element
+ * @param d direction to highlight in
+ */
+function highlight(d) {
+	let elements = getElements();
+	let columns = getColumns();
+
+	// Highlight the current element
+	if (d === DIRECTION.none) {
+		elements[STATE.selection].setAttribute("style", BACKGROUND_COLOUR);
+
+		// Remove the highlight from the current element
+	} else if (d === DIRECTION.remove) {
+		elements[STATE.selection].removeAttribute("style");
+
+		// Highlight forward but at the end of the elements
+	} else if (STATE.selection === elements.length - 1 && d === DIRECTION.forwards) {
+		return;
+
+		// Highlight backwards but at the start of the elements
+	} else if (STATE.selection === 0 && d === DIRECTION.backwards) {
+		return;
+
+		// Highlights the element in the row above or below
+	} else if (d === DIRECTION.up || d === DIRECTION.down) {
+		if (d === DIRECTION.up && STATE.selection < columns) {
+			return;
+		}
+		highlight(DIRECTION.remove);
+		let multiplier = d / 2;
+		if (d === DIRECTION.down) {
+			if (STATE.selection >= elements.length - columns) {
+				STATE.selection = elements.length - 1;
+				highlight(DIRECTION.none);
+				return;
+			}
+		}
+		STATE.selection += (columns * multiplier);
+		highlight(DIRECTION.none);
+
+		// Highlighting forwards or backwards with an element on the relevant side
+	} else {
+		highlight(DIRECTION.remove);
+		STATE.selection += d;
+		highlight(DIRECTION.none);
+	}
+
+	scroll();
+}
+
+/**
+ * 5 (select) was pressed
+ */
+function select() {
+	if (checkHome()) {
+
+	} else if (checkList()) {
+
+	} else if (checkShow()) {
+
+	}
+}
+
+/**
+ * 6 (right arrow) was pressed
+ */
+function right() {
+	if (checkHome()) {
+
+	} else if (checkList()) {
+
+	} else if (checkShow()) {
+
+	}
+}
+
+/**
+ * 4 (left arrow) was pressed
+ */
+function left() {
+	if (checkHome()) {
+
+	} else if (checkList()) {
+
+	} else if (checkShow()) {
+
+	}
+}
+
+/**
+ * 8 (up arrow) was pressed
+ */
+function up() {
+	if (checkHome()) {
+
+	} else if (checkList()) {
+
+	} else if (checkShow()) {
+
+	}
+}
+
+/**
+ * 2 (down arrow) was pressed
+ */
+function down() {
+	if (checkHome()) {
+
+	} else if (checkList()) {
+
+	} else if (checkShow()) {
+
+	}
+}
+
+/**
+ * Scrolls the page so the selected element if visible
+ */
+function scroll() {
+	let columns = getColumns();
+	let defaultPosition;
+	let elements = getElements();
+	if (checkHome()) {
+
+	} else if (checkList()) {
+
+	} else if (checkShow()) {
+
+	}
+
+	if (STATE.selection < columns) {
+		try {
+			defaultPosition.scrollIntoView();
+		} catch (TypeError) {
+			window.scrollTo(0, 0);
+		}
+	} else {
+		elements[STATE.selection - columns].scrollIntoView();
+	}
+}
+
+/**
+ * Selects the season menu
+ */
+function season() {
+
+}
+
+/**
+ * Toggles the play state of the video
+ */
+function playpause() {
+
+}
+
+/**
+ * Makes the video fullscreen
+ */
+function fullscreen() {
+
+}
+
+/**
+ * Closes the video if one is open, otherwise goes to the home page
+ */
+function back() {
+	if (checkWatch()) {
+
+	} else {
+		window.location = HOME_URL;
+	}
+}
+
+/**
+ * Navigates to the list page
+ */
+function list() {
+	window.location = LIST_URL;
+}
+
+/**
+ * Goes back to the media home page
+ */
+function home() {
+	window.location = "https://tarinnik.github.io/media/";
+}
+
+/**
+ * Refreshes the page
+ */
+function refresh() {
+	window.location = window.location.href;
+}
